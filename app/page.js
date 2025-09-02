7b2e732b6a316c77acdaca712019ebc9345ea59e
Dominic Vogl <dominic.vogl@cat-ia.de>
2025-09-02 21:42:55 +0200

    some content and copy improvements

--- app/page.js
+++ app/page.js
@@ -5,8 +5,11 @@ import FeatureGrid from "@/components/FeatureGrid";
 export default function Home() {
 	return (
 		<>
-			<h1 className={"text-2xl text-center font-bold"}>EANs scannen. Kopieren. Weiterarbeiten.</h1>
-			<h2 className={"mt-2 mb-6"}>Die kleine, schnelle Scanner-App: Scanne EAN-Codes und kopiere die gesammelte Liste mit einem Klick in deine persönlichen Tools.</h2>
+			<div className={"text-center mb-3"}>
+				<span className={"badge badge-primary badge-soft"}>Manga / Buch Scanner</span>
+			</div>
+			<h1 className={"text-2xl text-center font-bold"}>ISBN / EANs scannen. Kopieren. Weiterarbeiten.</h1>
+			<h2 className={"mt-2 mb-6"}>Deine smarte, schnelle Scanner-App: Scanne EAN-Codes und kopiere die gesammelte Liste mit einem Klick in deine persönlichen Tools.</h2>
 			<BarcodeScanner />
 			<FeatureGrid />
 		</>
